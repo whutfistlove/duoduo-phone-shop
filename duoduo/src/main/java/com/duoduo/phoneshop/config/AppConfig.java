7e4aa200ad28af5980a4f93ed043e4119a7d0ea5
Sumoothday <123644106+Sumoothday@users.noreply.github.com>
2025-07-21 00:08:14 +0800

    图片上传Bug修复

---
+++ duoduo/src/main/java/com/duoduo/phoneshop/config/AppConfig.java
@@ -0,0 +1,18 @@
+package com.duoduo.phoneshop.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class AppConfig {
+    private String uploadPath;
+
+    public String getUploadPath() {
+        return uploadPath;
+    }
+
+    public void setUploadPath(String uploadPath) {
+        this.uploadPath = uploadPath;
+    }
+}
